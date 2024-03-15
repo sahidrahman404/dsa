d8d824ac8d01984abb9d4ee7050cfd8a9b95a31c
Sahid Rahman <sahidrahman404@gmail.com>
2024-03-15 20:55:42 +0700

    add dynamicProgramming

---
+++ src/dynamicProgramming/fibonnaci.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, test } from "vitest";
+import { fib } from "./fibonnaci";
+
+describe("fibonnaci", () => {
+    test("fibonnaci 1", () => {
+        expect(fib(0)).toBe(0); // ->
+    });
+    test("fibonnaci 2", () => {
+        expect(fib(1)).toBe(1); // ->
+    });
+    test("fibonnaci 3", () => {
+        expect(fib(2)).toBe(1); // ->
+    });
+    test("fibonnaci 4", () => {
+        expect(fib(3)).toBe(2); // ->
+    });
+});
